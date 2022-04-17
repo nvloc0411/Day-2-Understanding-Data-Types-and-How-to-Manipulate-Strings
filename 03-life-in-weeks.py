fcea78f9b1b25bb1a938a57a915015b6f5122c0f
nvloc0411 <69301479+nvloc0411@users.noreply.github.com>
2022-04-17 09:02:00 +0700

    Create 03-life-in-weeks.py

---
+++ 03-life-in-weeks.py
@@ -0,0 +1,9 @@
+# 🚨 Don't change the code below 👇
+age = input("What is your current age?")
+# 🚨 Don't change the code above 👆
+
+days = 365 * (90 -int(age))
+weeks = 52 * (90 -int(age))
+months = 12 * (90 -int(age))
+print(f"You have {days} days, {weeks} weeks, and {months} months left")
+#Write your code below this line 👇
