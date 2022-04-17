76744aa12c8d7c1c8f5ba8214250420c23742568
nvloc0411 <69301479+nvloc0411@users.noreply.github.com>
2022-04-17 09:00:42 +0700

    Create 01-data-types.py

---
+++ 01-data-types.py
@@ -0,0 +1,8 @@
+# 🚨 Don't change the code below 👇
+two_digit_number = input("Type a two digit number: ")
+# 🚨 Don't change the code above 👆
+digit_one = two_digit_number[0]
+digit_two = two_digit_number[1]
+print(int(digit_one) + int(digit_two))
+####################################
+#Write your code below this line 👇
